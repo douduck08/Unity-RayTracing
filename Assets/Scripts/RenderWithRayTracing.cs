4bc7e1ab4c5304f088ef9274ecb9467a5c805653
douduck08 <lu10082000@gmail.com>
2019-01-07 18:28:44 +0800

    updating kernals

--- Assets/Scripts/RenderWithRayTracing.cs
+++ Assets/Scripts/RenderWithRayTracing.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 [RequireComponent (typeof (Camera))]
 public class RenderWithRayTracing : MonoBehaviour {
 
+    [SerializeField] ComputeShader rayTracingKernals;
+
     Camera mainCamera;
     RenderTexture renderTarget;
-    Vector3[] frustumCorners = new Vector3[4];
 
-    [SerializeField] ComputeShader rayTracingShader;
-    int kernelID = 0;
+    Vector3[] frustumCorners = new Vector3[4];
+    Vector4 cameraParameter;
+    Matrix4x4 cameraRayParameter;
+
+    int initCameraRaysKernelID;
+    int rayTracingKernelID;
+    int normalizeSamplesKernelID;
+
     ComputeBuffer sphereBuffer;
 
     void OnEnable () {
         mainCamera = GetComponent<Camera> ();
-        renderTarget = CreateRenderTarget (mainCamera.pixelWidth, mainCamera.pixelHeight);
-        rayTracingShader.SetTexture (kernelID, "result", renderTarget);
+        renderTarget = new RenderTexture (mainCamera.pixelWidth, mainCamera.pixelHeight, 0);
+        renderTarget.enableRandomWrite = true;
+        renderTarget.Create ();
+        rayTracingKernals.SetTexture (rayTracingKernelID, "result", renderTarget);
+
+        initCameraRaysKernelID = rayTracingKernals.FindKernel ("InitCameraRays");
+        rayTracingKernelID = rayTracingKernals.FindKernel ("RayTracing");
+        normalizeSamplesKernelID = rayTracingKernals.FindKernel ("NormalizeSamples");
 
         sphereBuffer = new ComputeBuffer (1000, RayTracingObjectDataSize.Sphere);
-        rayTracingShader.SetBuffer (kernelID, "sphereBuffer", sphereBuffer);
+        rayTracingKernals.SetBuffer (rayTracingKernelID, "sphereBuffer", sphereBuffer);
     }
 
     void OnDisable () {
@@ -28,47 +41,33 @@ public class RenderWithRayTracing : MonoBehaviour {
         }
     }
 
+    // render pipeline
     void OnPreRender () {
-        rayTracingShader.SetVector ("cameraParameter", GetCameraParameter (mainCamera.pixelWidth, mainCamera.pixelHeight));
-        rayTracingShader.SetMatrix ("cameraCorner", GetCameraCorner ());
+        UpdateCameraParameters ();
 
-        rayTracingShader.SetInt ("sphereNumber", RayTracingObjectManager.instance.sphereNumber);
+        rayTracingKernals.SetInt ("sphereNumber", RayTracingObjectManager.instance.sphereNumber);
         sphereBuffer.SetData (RayTracingObjectManager.instance.sphereDataArray);
+    }
 
-        rayTracingShader.Dispatch (kernelID, Mathf.CeilToInt (mainCamera.pixelWidth / 8.0f), Mathf.CeilToInt (mainCamera.pixelHeight / 8.0f), 1);
+    void OnRenderObject () {
+        rayTracingKernals.Dispatch (rayTracingKernelID, Mathf.CeilToInt (mainCamera.pixelWidth / 8.0f), Mathf.CeilToInt (mainCamera.pixelHeight / 8.0f), 1);
     }
 
     void OnRenderImage (RenderTexture source, RenderTexture dest) {
         Graphics.Blit (renderTarget, dest);
     }
 
-    void OnDrawGizmos () {
-        for (int i = 0; i < 4; i++) {
-            var worldSpaceCorner = transform.TransformVector (frustumCorners[i]);
-            Debug.DrawRay (transform.position, worldSpaceCorner, Color.blue);
-        }
-    }
+    // internal methods
+    void UpdateCameraParameters () {
+        int width = mainCamera.pixelWidth, height = mainCamera.pixelHeight;
+        cameraParameter = new Vector4 (width, height, 1f / width, 1f / height);
+        rayTracingKernals.SetVector ("cameraParameter", cameraParameter);
 
-    RenderTexture CreateRenderTarget (int width, int height) {
-        var rt = new RenderTexture (width, height, 0);
-        rt.enableRandomWrite = true;
-        rt.Create ();
-        return rt;
-    }
-
-    Vector4 GetCameraParameter (int width, int height) {
-        return new Vector4 (width, height, 1f / width, 1f / height);
-    }
-
-    Matrix4x4 GetCameraCorner () {
         mainCamera.CalculateFrustumCorners (new Rect (0, 0, 1, 1), mainCamera.farClipPlane, Camera.MonoOrStereoscopicEye.Mono, frustumCorners);
-
-        Matrix4x4 cameraCorner = new Matrix4x4 ();
-        cameraCorner.SetRow (0, transform.position);
-        cameraCorner.SetRow (1, Vector3.Normalize (frustumCorners[0]));
-        cameraCorner.SetRow (2, Vector3.Normalize (frustumCorners[2]) - Vector3.Normalize (frustumCorners[1]));
-        cameraCorner.SetRow (3, Vector3.Normalize (frustumCorners[1]) - Vector3.Normalize (frustumCorners[0]));
-
-        return cameraCorner;
+        cameraRayParameter.SetRow (0, transform.position);
+        cameraRayParameter.SetRow (1, Vector3.Normalize (frustumCorners[0]));
+        cameraRayParameter.SetRow (2, Vector3.Normalize (frustumCorners[3]) - Vector3.Normalize (frustumCorners[0]));
+        cameraRayParameter.SetRow (3, Vector3.Normalize (frustumCorners[1]) - Vector3.Normalize (frustumCorners[0]));
+        rayTracingKernals.SetMatrix ("cameraRayParameter", cameraRayParameter);
     }
 }
